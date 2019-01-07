2f6d69443f4d607f55630aff7055713a52520c99
Shemar Dacosta <dshemar6@gmail.com>
2019-01-07 04:52:28 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,9 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        :name => "Steven",
+        :age => "30",
+        :attitude => "confused"
       ]
    }, 
    :capulet => {
@@ -14,6 +17,9 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        :name => "Nurse"
+        :age => "44",
+        :attitude => "worried"
       ]
    }
 }
